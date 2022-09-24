1b3c3cdf3474632b1a4d077fb2589a99266b055f
wiz <arifard49@gmail.com>
2022-09-24 15:11:18 +0700

    menambahkan config i3, ranger, rofi, dan nvim

---
+++ config/nvim/lua/wiz/bufferline.lua
@@ -0,0 +1,35 @@
+require('bufferline').setup{
+  options = {
+    mode = "buffers",
+    separator_style = 'thick',
+    offsets = { { filetype = "NvimTree",
+                  text = "File Explorer",
+                  text_align = 'left',
+                  highlight = "Directory",
+                  separator = true } },
+    always_show_bufferline = true,
+    show_buffer_close_icons = true,
+    show_close_icon = false,
+    close_command = "bdelete! %d",
+    color_icons = true,
+  },
+  highlights = {
+    separator = {
+      fg = '#073642',
+      bg = '#073642',
+    },
+    separator_selected = {
+      fg = '#073642',
+    },
+    buffer_selected = {
+      fg = '#fdf6e3',
+      bold = true,
+      --undercurl = true,
+      underline = true,
+      sp = '#fdf6e3'
+    },
+    fill = {
+      bg = '#073642',
+    },  
+  }, 
+}
